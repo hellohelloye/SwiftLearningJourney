8921e975b752d09650ed6cf187de192639f0a821
Yukui Ye <mollysmile.ye@gmail.com>
2014-06-16 16:39:01 -0400

    add cocoopod

---
+++ WhereToTalk/WhereToTalk/WhereToTalk-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WhereToTalk-Bridging-Header.h
+//  WhereToTalk
+//
+//  Created by Yukui ye on 6/16/14.
+//  Copyright (c) 2014 M.Shanken. All rights reserved.
+//
+
+#ifdef __OBJC__
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <Mantle/Mantle.h>
+
+#endif
